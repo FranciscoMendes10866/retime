42a4d71e1742d40e513f025da4e7d27e3f0ddd8e
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2023-11-26 17:37:50 +0000

    removed unused files

--- app/utils/parseJSON.ts
+++
@@ -1,8 +0,0 @@
-export function parseJSON<T>(value: string | null): T | undefined {
-  try {
-    return value === "undefined" ? undefined : JSON.parse(value ?? "");
-  } catch {
-    console.log("parsing error on", { value });
-    return undefined;
-  }
-}
